b0dced8106bdd2a06437e967b1434199581848b9
Alejandro Romero <arome90@users.noreply.github.com>
2023-06-27 11:37:13 +0200

    OnEnrterRotate

---
+++ Assets/Scripts/onEnterRotate.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class onEnterRotate : MonoBehaviour
+{
+
+    [SerializeField] private GameObject[] activeElements;
+    [SerializeField] private GameObject[] rotateElements;
+
+    private bool areRotating = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (areRotating)
+        {
+            foreach (var element in rotateElements)
+            {
+                element.transform.Rotate(0, 50 * Time.deltaTime,0 );
+            }
+        }
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        foreach (var element in activeElements)
+        {
+            element.SetActive(true);
+        }
+        this.areRotating = true;
+    }
+}
