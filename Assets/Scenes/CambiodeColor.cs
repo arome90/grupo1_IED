be6fb99abdb5f134678cc7c3eca9f323411609bf
Paola <paobandam@gmail.com>
2023-06-29 13:40:02 +0200

    Cambio Color

---
+++ Assets/Scenes/CambiodeColor.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CambiodeColor : MonoBehaviour
+{
+        public Material materialCuboVerde;
+        public Material materialCuboRojo;
+
+        public MeshRenderer mesh;
+
+        public void CambioMaterialVerde()
+        {
+            mesh.material = materialCuboVerde;
+        }
+        public void CambioMaterialRojo()
+        {
+            mesh.material = materialCuboRojo;
+        }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            CambioMaterialVerde();
+        }
+
+        private void OnTriggerExit(Collider other)
+        {
+            CambioMaterialRojo();
+        }
+}
